93d8f3f57ea8261da1c243e3b0428697ce356832
@charithakaush <charithakaush@gmail.com>
2024-08-03 07:00:38 +0200

    sign in

--- tests/end_to_end/test_homepage.py
+++ tests/end_to_end/test_homepage.py
@@ -10,10 +10,10 @@ def test_homepage(home_page):
     page.click('button:has-text("Accept")')  # Example button click, adjust the selector as needed
     page.click('a:has-text("Sign in")')
     page.get_by_label("Email").fill("charith.sliit@gmail.com")
-    page.get_by_placeholder("Password").fill("123456789")
+    page.get_by_placeholder("Password").fill("6Jig.,9.JeeMgU:")
     page.get_by_role("button", name="Submit").click()
-    page.get_by_text("Invalid Email or password.").click()
-    page.get_by_role("link", name="Create an Account.").click()
+    page.wait_for_timeout(5000)
+
 
 
    
